27a5c3864055050dc893f19c7271fc1587cd6036
Andy Lau <MiuLun.Lau@INL.gov>
2017-06-07 12:52:23 -0600

    1.2 Working VTK

--- python_scripts/add.py
+++ python_scripts/add.py
@@ -1,7 +1,10 @@
 import runtest
 
+# IMPORT INPUT FILENAME
 inname=runtest.inname
+outname=inname[:-4]+'.1.vtk'
 
+# READ INPUT FILE
 with open(inname) as f:
     out=[]
     for line in f:
@@ -10,24 +13,34 @@ with open(inname) as f:
             line=[str(i) for i in line]  # convert to str
             out.append(line)
 
+# COUNTS THE NUMBER OF ROWS AND COLUMNS
 NumsRows=len(out)
 NumsColu=(len(out[0]))
 
+# NUMBER OF ATTRIBUTES
+NumsA=NumsColu-3
+
+# FORMAT ATTRIBUTES 
 x=[]
-for j in range(NumsRows):
+for i in range(NumsRows):
     x.append([])
-    x[j]=out[j][3]
+    for j in range(NumsA):
+        x[i].append(0)
+        x[i][j]=out[i][j+3]
+
+# OUTPUT INTO VTK
+with open (outname, "a+") as f:
+    f.write('\n')
+    M= 'POINT_DATA ' + str(NumsRows)+ '\n'
+    f.write(M)
+    for j in range(NumsA):
+        M='SCALARS Scalars_'+str(j+1)+' float \nLOOKUP_TABLE default \n'
+        f.write(M)
+        for i in range(NumsRows):
+            f.write(x[i][j])
+            f.write('\n')
 
 
-with open('Gamma.1.vtk', "a+") as f:
-    M='POINT_DATA '+str(NumsRows)+ '\n'
-    f.write(M)
-    M='SCALARS cell_scalars float 1 \nLOOKUP_TABLE default \n'
-    f.write(M)
-    for i in range(NumsRows):
-        f.write(x[i])
-        f.write('\n')
-    
    
 
 
