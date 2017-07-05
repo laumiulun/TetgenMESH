8eb2decb0a605fde950731a168d5c133529b550d
Andy Lau <MiuLun.Lau@INL.gov>
2017-07-05 08:19:34 -0600

    Added integration to designated rockworks format

--- python_scripts/runtest.py
+++ python_scripts/runtest.py
@@ -19,47 +19,202 @@ class bcolors:
     RED = '\033[91m'        #RED
     GREEN = '\033[42m'      #GREEN
     
-# Function to output error
+# FUNCTION TO OUTPUT ERROR
 def fileERROR(command,message):
     try:
         f= open(command)
     except FileNotFoundError:
-        print(bcolors.RED+'ERROR: ' +str(message) +' Not Found'+bcolors.N)
-        exit()
+        print(bcolors.RED+'ERROR: ' +str(message) +' NOT FOUND'+bcolors.N)
+        sys.exit()
+        
+# FUNCTION REDUNDANCY
+def redundancy(filetocheck):
+    for i in range(len(filetocheck)-1):
+        
+        #OPEN first file:
+        with open(filetocheck[i]) as f:
+            out=[]
+            for line in f:
+                line = line.split()
+                if line:
+                    line=[str(i) for i in line]  # convert to str
+                    out.append(line)
+        del out[0]
+        
+        #OPEN second file:
+        with open(filetocheck[i+1]) as f:
+            out2=[]
+            for line in f:
+                line = line.split()
+                if line:
+                    line=[str(i) for i in line]  # convert to str
+                    out2.append(line)
+        del out2[0]                   
 
+        # Compare the 2 files
+        for i in range(len(out)):
+            for j in range(3):
+                if out[i][j] == out2[i][j]:
+                    continue
+                else:
+                    print('ERROR, XYZ DOES NOT MATCH')
+                    print(os.path.basename(filetocheck[i+1]),' COORDINATES DOES NOT MATCH')
+                    sys.exit()
+    print('REDUNDANCY CHECK OK')
+
+def readfile(filenameinput):
+    with open(filenameinput) as f:
+        out=[]
+        for line in f:
+            line = line.split()
+            if line:
+                line=[str(i) for i in line]  # convert to str
+                out.append(line)
+    return out
+
+# START OF THE SCRIPTS
+#############################################################################
+
+print("-"*50)
 # GET CURRENT WORKING DIRECTORY AND MOVE INTO TEST DIRECTORY
 os2=os.getcwd()   # OS2: Current working directory
 
 # ASK FOR USER INPUT TO THE FILE
 usrname=input(bcolors.BOLD+'Enter Input File Name: '+bcolors.N)
 inname=os.path.join(os2,usrname)
+
+print('usrname:',usrname)
+print('inname:',inname)
+
+# Find files
+i=0
+j=0
+x=[]
+for root, dirs, files in os.walk(os2, topdown=True):    # Find all files in the the dir
+    for name in files:
+        if usrname in name:
+            j+=1
+            x.append([])
+            x[i-1]=(os.path.join(root, name))
+
 #print(bcolors.BOLD+'Input WD:'+ bcolors.N + inname)
 
 f=inname.find(".")
-outname=inname[0:f]+'.node'
+outname=inname[0:f]+'.node'  # print .node absolute filepath
 
 #print(bcolors.BOLD+'Output WD:'+bcolors.N + outname)
 
-# FILE NOT FIND ERROR
+#FILE NOT FIND ERROR
 fileERROR(inname,"INPUT FILE")
 
 # OPEN INPUT FILE(TXT)
-with open(inname) as f:
-    out=[]
-    for line in f:
-        line = line.split()
-        if line:
-            line=[str(i) for i in line]  # convert to str
-            out.append(line)
-#del out[0]
-list = out
-locat=int(findlocation('END'))
+##with open(inname) as f:
+##    out=[]
+##    for line in f:
+##        line = line.split()
+##        if line:
+##            line=[str(i) for i in line]  # convert to str
+##            out.append(line)
 
-del out[0:locat+1]
+out=readfile(inname)
+
+
+
+"""-------------------------------------------------------------------------------------------------------------"""
+# FILE FORMAT CHECK
+# ASK FOR USER INPUT FORMAT
+formatname=input(bcolors.BOLD+"ENTER FORMATS(ROCKWORKS OR PETREL): " + bcolors.N)
+
+if formatname == "ROCKWORKS":
+    del out[0]
+
+    n=usrname.find('.')
+    string=usrname[:n]  # remove .txt and attribute subset
+    fileformat='.txt'
+    # Find files
+    i=0
+    j=0
+    x=[]
+    for root, dirs, files in os.walk(os2, topdown=True):    # Find all files in the the dir
+        for name in files:
+            if string in name and fileformat in name:
+                j+=1
+                x.append([])
+                x[i-1]=(os.path.join(root,name))
+            
+    # Call redundancy FUNCTION
+    redundancy(x)
+
+    # Extract XYZ data
+    with open(x[0]) as f:
+            out=[]
+            for line in f:
+                line = line.split()
+                if line:
+                    line=[str(i) for i in line]  # convert to str
+                    out.append(line)
+            del out[0]
+    xyz=[]
+    for i in range(len(out)):
+        xyz.append([])
+        for j in range(3):
+            xyz[i].append(0)
+            xyz[i][j]=float(out[i][j])
+
+
+    # Combine into one VTK
+    att=[]
+    for i in range(len(x)):
+
+        # OPEN AND READ FILES
+        with open(x[i]) as f:
+            out=[]
+            for line in f:
+                line = line.split()
+                if line:
+                    line=[str(i) for i in line]  # convert to str
+                    out.append(line)
+            del out[0]
+
+        # EXTRACT ATT VALUES
+        att.append([])
+        for k in range(len(out)):
+            att[i].append(0)
+            att[i][k]=float(out[k][3])
+
+    # Transpose the att
+    tatt=[list(x) for x in zip(*att)]
+
+    # Combine xyz and attributes
+    sourceout=[]
+    for i in range(len(xyz)):
+        sourceout.append([])
+        sourceout[i]=xyz[i]+tatt[i]
+
+    with open (outname,"w+") as f:
+        
+        for i in range(len(sourceout)):
+            for j in range(len(sourceout[0])):
+                f.write(str(sourceout[i][j]))
+                f.write(' ')
+            f.write('\n')
+
+    inname=outname
+elif formatname == "PETREL":
+    list = out
+    locat=int(findlocation('END'))
+    del out[0:locat+1]
+    
+else:
+    print(bcolors.RED +"ERROR: FILE FORMAT NOT FOUND" + bcolors.N)
+    exit()
+
+"""-------------------------------------------------------------------------------------------------------------"""
 
 # NUMBER OF ROWS AND COLUMNS
 NumsRows=len(out)
 NumsColu=(len(out[0]))
+
 print('Reading Input File'+"."*15,end="")
 
 # ORGANIZE DATA INTO COLUMNS AND ROWS
@@ -97,40 +252,34 @@ print('Number of Nodes:',NumsRows)
 print(bcolors.BOLD+"CALLING TETGEN..."+bcolors.N)
 print("-"*50)
 
-os3m=os.path.basename(os2)
-os3=os2[:-len(os3m)]
+#FIND TETGEN DIR
+os5=os.path.dirname(os.path.dirname(os2))
+tetgen='tetgen.exe'
+for root, dirs, files in os.walk(os5, topdown=True):    # Find all files in the the dir
+    for name in files:
+        if tetgen in name:
+            pathtotetgen=(os.path.join(root, name))
 
-
-pathtotetgen=os.path.join(os3,'TetgenMesh/tpl/tetgen/tetgen')
+# 
 output=(pathtotetgen + " -kNEF " + outname)
 
-subprocess.call(output,shell=True)
 
+subprocess.call(output,shell=True)
 print(bcolors.GREEN+'Tetgen OK'+bcolors.N)
 
 """-------------------------------------------------------------------------------------------------------------"""
 
 outname2=outname[:-4]+'1.vtk'
 
-# READ INPUT FILE
-with open(inname) as f:
-    out=[]
-    for line in f:
-        line = line.split()
-        if line:
-            line=[str(i) for i in line]  # convert to str
-            out.append(line)
-            
-del out[0:locat+1]
-# COUNTS THE NUMBER OF ROWS AND COLUMNS
-NumsRows=len(out)
-NumsColu=(len(out[0]))
+if formatname == 'PETREL':
+    NullV=int(-998)
+elif formatname == 'ROCKWORKS':
+    NullV=int(-10000)
 
-NullV=int(-998)
-# NUMBER OF ATTRIBUTES
+# NUMBER OF ATTRIBUTES (subtracting XYZ)
 NumsA=NumsColu-3
 
-# FORMAT ATTRIBUTES 
+# FORMAT ATTRIBUTES
 x=[]
 for i in range(NumsRows):
     x.append([])
@@ -164,6 +313,3 @@ with open (outname2, "a+") as f:
 
 print("-"*50)
 print(bcolors.GREEN+bcolors.BOLD+"Finish"+bcolors.N)
-
-
-
