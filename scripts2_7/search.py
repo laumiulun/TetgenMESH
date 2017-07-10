986ec7febce2f232e6767be52b5777a96c4e241a
Andy Lau <MiuLun.Lau@INL.gov>
2017-07-10 09:28:27 -0600

    Remove script2_7, standarize python2 as the main developing platform

--- scripts2_7/search.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def find(name):
-    for root, dirs, files in os.walk(os.getcwd()):
-        if name in files:
-            return os.path.join(root, name)
-        
-
-print (find('tetgen'))
